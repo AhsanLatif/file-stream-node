f1738a52d855604c9ad1fd01f1c4bf69b4fe9273
Ahsan Latif <ahsanlatif18@gmail.com>
2022-07-12 02:06:26 +0500

    implementation using transform method

--- logparser.js
+++ logparser.js
@@ -1,61 +1,59 @@
-const readline = require('readline');
 const myargs = process.argv.slice(2);
 const inputfile = myargs[1];
 const outputfile = myargs[3];
-// console.log(inputfile)
-// console.log(outputfile)
+
 var fs = require('fs'),
     path = require('path'),    
-    filePath = path.join(__dirname, inputfile);
-    console.log(filePath);
+    inputFilePath = path.join(__dirname, inputfile),
+    outputFilePath = path.join(__dirname, outputfile);
 
-const file = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    output: process.stdout,
-    terminal: false
-});
+const stream = require('stream');
+const readStream = fs.createReadStream(inputFilePath);  
+const writeStream = fs.createWriteStream(outputFilePath);
 
-file.on('line', (line) => {
-    let data = line.split(' - ');
-    let transactionDetail = JSON.parse(data[2])
-    const date = new Date(data[0]);
-    const timestamp = date.getTime();
-    let log = {
-        "timestamp": timestamp,
-        "loglevel": data[1],
-        "transactionId": transactionDetail['transactionId'],
-        "err": transactionDetail['details']
-    }
-    console.log(log);
-});
+var xtream = new stream.Transform( { objectMode: true } );
 
-// {"timestamp":1628475171259,"loglevel":"error","transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","err":"Not found"}
+xtream._transform = function(chunk, encoding, done) {
+	var strData = chunk.toString();
 
+	if (this._invalidLine) {
+		strData = this._invalidLine + strData;
+	};
 
-// [{"timestamp": <Epoch Unix Timestamp>, "loglevel": "<loglevel>", "transactionId: "<UUID>", "err": "<Error message>" }]
+	var objLines = strData.split("\n"); 
+	this._invalidLine = objLines.splice(objLines.length-1, 1)[0];  
+    console.log(this._invalidLine);
+    let jsonError = '';
+    objLines.forEach(line => {
+        let data = line.split(' - ');
+        let transactionDetail = JSON.parse(data[2])
+        const date = new Date(data[0]);
+        const timestamp = date.getTime();
+        if (data[1] === 'error'){
+            let log = {
+                "timestamp": timestamp,
+                "loglevel": data[1],
+                "transactionId": transactionDetail['transactionId'],
+                "err": transactionDetail['details']
+            }
+            let sentence = JSON.stringify(log);
+            jsonError = jsonError + sentence;
+        }
+    });
+   
+	this.push(jsonError);
 
-// let abc = [1,2,3,4,5];
-// // abc.forEach(item => {
-// //     console.log(item)
-// // })
-// let ghi = {}
-// for (let item of abc){
-//     // console.log(item)
-// }
+	done();
+};
 
-// for (let item in abc){
-//     ghi[item] = 'test';
-    
-// }
+xtream._flush = function(done) {
+	if (this._invalidLine) {   
+		this.push(this._invalidLine); 
+	};
 
-// // for (let item in ghi){
+	this._invalidLine = null;
+	done();
+};
 
-// //     console.log(ghi[item])
-// // }
-
-// let res = abc.map(item => {
-//     return item * 2;
-    
-// })
-
-// console.log(res)
+readStream.pipe(xtream).pipe(writeStream).on('finish', () => {
+        });
